1177ab7409d62e2499cafb6307f2b4b912568c9f
SinniS1 <sinniscode@gmail.com>
2023-05-31 15:07:52 +0530

    🚀

--- button-component/src/components/Footer.jsx
+++ button-component/src/components/Footer.jsx
@@ -1,7 +1,12 @@
 const Footer = () => {
   return (
     <footer className='Footer'>
-      <span style={{ fontWeight: 'bold' }}>Made by SinniS </span>
+      <span style={{ fontWeight: 'bold' }}>
+        Made by &nbsp;
+        <a target='_blank' rel='noreferrer' href='https://github.com/SinniS1'>
+          SinniS
+        </a>
+      </span>
     </footer>
   )
 }
